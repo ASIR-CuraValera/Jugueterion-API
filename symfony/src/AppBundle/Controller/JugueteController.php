84bb185c8a7e6d0f34236c212f11f51e7f117384
Ikillnukes <ikillnukes1@gmail.com>
2018-06-16 20:33:57 +0200

    JugueteController

---
+++ symfony/src/AppBundle/Controller/JugueteController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Álvaro
+ * Date: 16/06/2018
+ * Time: 20:28
+ */
+
+namespace AppBundle\Controller;
+
+
+class JugueteController
+{
+    public function pruebasAction() {
+        die("Hola jueguetes!!");
+    }
+}
